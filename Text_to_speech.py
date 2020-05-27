f2f5648b2f09b97f317cb3a947507e98f3258763
Vasant Kumar Desai <33997408+vpdesai2020@users.noreply.github.com>
2020-05-27 23:23:50 +0530

    Add files via upload

---
+++ Text_to_speech.py
@@ -0,0 +1,17 @@
+from gtts import gTTS
+import os
+
+
+def texttospeech():
+    try:
+        Text= "Hello! Welcome to GitHub!"
+        language='en'
+        speech=gTTS(Text,lang=language, slow=False)
+        speech.save("test.mp3")
+        os.system("start test.mp3")
+
+    except UnicodeDecodeError:
+        print("Some characters are not supported")
+
+texttospeech()
+
