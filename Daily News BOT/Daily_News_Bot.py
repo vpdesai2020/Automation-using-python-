ec24e743ef08c1cd12e4be8764732e6866d11c45
Vasant Kumar Desai <33997408+vpdesai2020@users.noreply.github.com>
2020-06-16 14:12:22 +0530

    Add files via upload

---
+++ Daily News BOT/Daily_News_Bot.py
@@ -0,0 +1,26 @@
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+from bs4 import BeautifulSoup
+
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          ConversationHandler, InlineQueryHandler)
+
+
+def start(bot, update):
+    source=requests.get('https://inshorts.com/en/read').text
+    soup=BeautifulSoup(source,'lxml')
+    for news in soup.find_all('div', class_='news-card-title news-right-box'):
+        headings=news.span.text
+        update.message.reply_text(headings)                            
+
+def main():
+    updater = Updater('847610448:AAGjGQ5jJxwdYtvKQyzk2x4NFTHmFe6w9ko')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start',start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
